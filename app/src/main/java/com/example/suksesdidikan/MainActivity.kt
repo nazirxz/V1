e3e3f24240022d3f9ca3089d7e8977bd5442f875
nazirxz <nzryanzr@gmail.com>
2023-12-31 15:21:47 +0700

    adding navigation drawer

--- app/src/main/java/com/example/suksesdidikan/MainActivity.kt
+++ app/src/main/java/com/example/suksesdidikan/MainActivity.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.suksesdidikan.databinding.ActivityMainBinding
 
-class MainActivity :AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val userName = intent.getStringExtra("USER_NAME")
         val welcomeText = "Hello $userName"
         binding.tvWelcome.text = welcomeText
 
-
+        // Mengatur klik listener pada tombol drawer
+        binding.drawer.setOnClickListener {
+            // Menampilkan atau menyembunyikan navigation drawer sesuai status saat ini
+            if (binding.drawerLayout.isDrawerOpen(binding.navigationView)) {
+                binding.drawerLayout.closeDrawer(binding.navigationView)
+            } else {
+                binding.drawerLayout.openDrawer(binding.navigationView)
+            }
+        }
     }
 }
